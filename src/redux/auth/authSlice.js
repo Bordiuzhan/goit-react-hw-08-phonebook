7e45f611231dc6162c7bdc7dbc91bc60e3ee569b
Bordiuzhan <bordyujan@gmail.com>
2023-01-26 01:22:01 +0100

    changed to addCase extrareducers

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,6 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
 
+const logInFulfilled = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+};
+const logOutFulfilled = (state, action) => {
+  state.user = { name: null, email: null };
+  state.token = null;
+  state.isLoggedIn = false;
+};
+const refreshUserFulfilled = (state, action) => {
+  state.user = action.payload;
+  state.isLoggedIn = true;
+  state.isRefreshing = false;
+};
+const refreshUserPending = state => {
+  state.isRefreshing = true;
+};
+const refreshUserRejected = state => {
+  state.isRefreshing = false;
+};
+
 const initialState = {
   user: { name: null, email: null },
   token: null,
@@ -11,34 +33,14 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {
-    [register.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logIn.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logOut.fulfilled](state) {
-      state.user = { name: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-    },
-    [refreshUser.pending](state) {
-      state.isRefreshing = true;
-    },
-    [refreshUser.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
-    },
-    [refreshUser.rejected](state) {
-      state.isRefreshing = false;
-    },
-  },
+  extraReducers: builder =>
+    builder
+      .addCase(register.fulfilled, logInFulfilled)
+      .addCase(logIn.fulfilled, logInFulfilled)
+      .addCase(logOut.fulfilled, logOutFulfilled)
+      .addCase(refreshUser.pending, refreshUserPending)
+      .addCase(refreshUser.fulfilled, refreshUserFulfilled)
+      .addCase(refreshUser.rejected, refreshUserRejected),
 });
 
 export const authReducer = authSlice.reducer;
--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -28,14 +28,6 @@ const anyFulfilledReducer = state => {
   state.error = null;
 };
 
-// const handlePending = state => {
-//   state.isLoading = true;
-// };
-// const handleRejected = (state, action) => {
-//   state.isLoading = false;
-//   state.error = action.payload;
-// };
-
 const phoneInitialState = {
   contacts: [],
   isLoading: false,
@@ -53,33 +45,6 @@ const contactsSlice = createSlice({
       .addMatcher(getAction('pending'), anyPendingReducer)
       .addMatcher(getAction('rejected'), anyRejectedReducer)
       .addMatcher(getAction('fulfilled'), anyFulfilledReducer),
-
-  // extraReducers: {
-  //   [fetchPhones.pending]: handlePending,
-  //   [fetchPhones.fulfilled](state, action) {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //     state.contacts = action.payload;
-  //   },
-  //   [fetchPhones.rejected]: handleRejected,
-  //   [AddContact.pending]: handlePending,
-  //   [AddContact.fulfilled](state, action) {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //     state.contacts.push(action.payload);
-  //   },
-  //   [AddContact.rejected]: handleRejected,
-  //   [DeleteContact.pending]: handlePending,
-  //   [DeleteContact.fulfilled](state, action) {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //     const index = state.contacts.findIndex(
-  //       contact => contact.id === action.payload.id
-  //     );
-  //     state.contacts.splice(index, 1);
-  //   },
-  //   [DeleteContact.rejected]: handleRejected,
-  // },
 });
 
 export const contactsReducer = contactsSlice.reducer;
